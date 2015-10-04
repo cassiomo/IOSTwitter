79656add8e43ce6fcb03c8955cdec47070baf587
Cassio Mo <cassiomo@gmail.com>
2015-10-04 16:52:22 -0700

    refactor

--- IOSTwitter/TwitterClient.swift
+++ IOSTwitter/TwitterClient.swift
@@ -14,11 +14,30 @@ let twitterBaseURL = NSURL(string: "https://api.twitter.com")
 
 class TwitterClient: BDBOAuth1RequestOperationManager {
     
+    var loginCompletion: ((user: User?, error: NSError?) -> ())?
+    
     class var sharedInstance : TwitterClient {
         struct Static {
             static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
         }
         return Static.instance
+    }
+    
+    func loginWithCompletion(completion: (user: User?, error: NSError?) -> ()) {
+        loginCompletion = completion
+        
+        
+        // fetch request token & redirect to authorization page
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential!) -> Void in
+            print("Got the request token")
+            var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            UIApplication.sharedApplication().openURL(authURL!)
+            
+            }) {
+                (error: NSError!) -> Void in
+                print("Failed to get request token")
+        }
     }
 
 }
---
+++ IOSTwitter/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  IOSTwitter
+//
+//  Created by Mo, Kevin on 10/4/15.
+//  Copyright © 2015 Mo, Kevin. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name : String?
+    var screenname: String?
+    var profileImageUrl: String?
+    var tagline: String?
+    var dictionary: NSDictionary
+    
+    init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
+        name = dictionary["name"] as? String
+        screenname = dictionary["screen_name"] as? String
+        profileImageUrl = dictionary["profile_image_url"] as? String
+        tagline = dictionary["description"] as? String
+    }
+}
